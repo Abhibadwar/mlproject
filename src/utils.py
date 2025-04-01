ceddccca0fd327dd93d3949a8b7e765767409102
Abhibadwar <98725240+Abhibadwar@users.noreply.github.com>
2025-04-01 12:25:29 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -29,7 +30,12 @@ def evaluate_models(X_train, y_train,X_test,y_test ,models):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            #param=param[list(models.keys())[i]]
 
+            #gs = GridSearchCV(model,param,cv=3)
+            #gs.fit(X_train,y_train)
+
+            #Smodel.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
@@ -47,6 +53,15 @@ def evaluate_models(X_train, y_train,X_test,y_test ,models):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+        try:
+            with open(file_path,"rb") as file_obj:
+                return dill.load(file_obj)
+            
+        except Exception as e:
+            raise CustomException(e,sys)
 
 
 
